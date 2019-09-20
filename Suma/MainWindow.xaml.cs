7b4de467f0c2ce5f08ff6544e529b32b52dd950b
rubenbc7 <rubenborbollacastelo@hotmail.com>
2019-09-19 20:19:23 -0700

    Agregar archivos de proyecto.

---
+++ Suma/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Suma
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Background = Brushes.Blue;
+        }
+
+        private void BtnSumar_Click(object sender, RoutedEventArgs e)
+        {
+            int numero1 = int.Parse (txtNumero1.Text);
+            int numero2 = int.Parse(txtNUmero2.Text);
+            int resultado = numero1 + numero2;
+            lblResultado.Text = resultado.ToString();
+        }
+    }
+}
